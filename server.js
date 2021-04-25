c6884082aa39baa44551eabe92b568e23ce503cd
vittaldummu <vittalvarun96@gmail.com>
2021-04-25 21:52:03 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require("http");
+const app = require("./app");
+const server = http.createServer(app);
+require("./database/db"); //import the database
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const port = process.env.PORT || 3031;
+
+server.listen(port, () => {
+  console.log(`Server is running on http://172.31.1.63:${port}`);
+});
+
